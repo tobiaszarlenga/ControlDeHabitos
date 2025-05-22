a2bc42178b2e6376c7c2a651b61bf3ec2c5f946d
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2025-05-21 21:47:20 -0300

    fix. error de vs

---
+++ ControlDeHabitos.API/Interfaces/IHabitoService.cs
@@ -0,0 +1,14 @@
+﻿using ControlDeHabitos.API.Models;
+
+namespace ControlDeHabitos.API.Interfaces
+{
+    public class IHabitoService
+    {
+        List<Habito> ObtenerTodos();
+        Habito? ObtenerPorId(int id);
+        Habito Crear(Habito nuevoHabito);
+        bool Actualizar(int id, Habito habitoActualizado);
+        bool Eliminar (int id);
+
+    }
+}
---
+++ ControlDeHabitos.Desktop/Form1.cs
@@ -0,0 +1,10 @@
+namespace ControlDeHabitos.Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
